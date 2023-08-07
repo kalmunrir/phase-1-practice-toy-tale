3bda01411309d8801d0291ef1a3f5d732b303b8d
Juan Ortiz-Acosta <jdoa2003@gmail.com>
2023-08-07 11:42:24 -0600

    Finished Lab

--- src/index.js
+++ src/index.js
@@ -12,4 +12,68 @@ document.addEventListener("DOMContentLoaded", () => {
       toyFormContainer.style.display = "none";
     }
   });
+
+  makeToyCards();
 });
+
+function makeToyCards() {
+  fetch(`http://localhost:3000/toys`)
+    .then(r => r.json())
+    .then(toys => {
+      // console.log(toys);
+      toys.forEach(toy => {
+        const div = document.createElement(`div`);
+        div.className = `card`;
+        const h2 = document.createElement(`h2`);
+        h2.textContent = toy['name'];
+        const img = document.createElement(`img`);
+        img.src = toy[`image`];
+        img.className = `toy-avatar`;
+        const p = document.createElement(`p`);
+        p.textContent = `${toy[`likes`]} Likes`;
+        const btn = document.createElement(`button`);
+        btn.className = `like-btn`;
+        btn.id = toy[`id`];
+
+        div.append(h2, img, p, btn);
+        document.querySelector(`div#toy-collection`).append(div);
+
+        btn.addEventListener(`click`, () => {
+          toy[`likes`] ++;
+          p.textContent = `${toy[`likes`]} Likes`;
+          updateToyLikes(toy)
+        })
+      })
+      
+    })
+}
+
+function postNewToy(toyName, toyImg) {
+  fetch("http://localhost:3000/toys", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            "Accept": "application/json",
+        },
+        body: JSON.stringify({
+            name: toyName,
+            image: toyImg,
+            likes: 0,
+        }),
+    })
+    .then(r => r.json())
+    .then(toys => makeToyCards())
+}
+
+function updateToyLikes(toyObj) {
+  fetch(`http://localhost:3000/toys/${toyObj[`id`]}`, {
+        method: "PATCH",
+        headers: {
+            "Content-Type": "application/json",
+            "Accept": "application/json",
+        },
+        body: JSON.stringify(toyObj)
+    })
+    .then(r => r.json())
+    .then(toy => console.log(toy))
+}
