eec4084965e4244912eca99f0a3a97fb8484f6bb
Juan Ortiz-Acosta <jdoa2003@gmail.com>
2023-08-07 12:46:46 -0600

    Actually Finished Lab

--- src/index.js
+++ src/index.js
@@ -8,6 +8,7 @@ document.addEventListener("DOMContentLoaded", () => {
     addToy = !addToy;
     if (addToy) {
       toyFormContainer.style.display = "block";
+      document.querySelector(`.add-toy-form`).addEventListener(`submit`, addToyHandler)
     } else {
       toyFormContainer.style.display = "none";
     }
@@ -21,31 +22,17 @@ function makeToyCards() {
     .then(r => r.json())
     .then(toys => {
       // console.log(toys);
-      toys.forEach(toy => {
-        const div = document.createElement(`div`);
-        div.className = `card`;
-        const h2 = document.createElement(`h2`);
-        h2.textContent = toy['name'];
-        const img = document.createElement(`img`);
-        img.src = toy[`image`];
-        img.className = `toy-avatar`;
-        const p = document.createElement(`p`);
-        p.textContent = `${toy[`likes`]} Likes`;
-        const btn = document.createElement(`button`);
-        btn.className = `like-btn`;
-        btn.id = toy[`id`];
-
-        div.append(h2, img, p, btn);
-        document.querySelector(`div#toy-collection`).append(div);
-
-        btn.addEventListener(`click`, () => {
-          toy[`likes`] ++;
-          p.textContent = `${toy[`likes`]} Likes`;
-          updateToyLikes(toy)
-        })
-      })
+      toys.forEach(toy => makeToyCard(toy))
       
     })
+}
+
+function addToyHandler (e) {
+  e.preventDefault();
+  const nameInput = document.querySelector(`#name`);
+  const imgInput = document.querySelector(`#image`);
+
+  postNewToy(nameInput.value, imgInput.value)
 }
 
 function postNewToy(toyName, toyImg) {
@@ -62,7 +49,7 @@ function postNewToy(toyName, toyImg) {
         }),
     })
     .then(r => r.json())
-    .then(toys => makeToyCards())
+    .then(toys => makeToyCard(toys))
 }
 
 function updateToyLikes(toyObj) {
@@ -76,4 +63,27 @@ function updateToyLikes(toyObj) {
     })
     .then(r => r.json())
     .then(toy => console.log(toy))
+}
+
+function makeToyCard (toyObj) {
+  const div = document.createElement(`div`);
+  div.className = `card`;
+  const h2 = document.createElement(`h2`);
+  h2.textContent = toyObj['name'];
+  const img = document.createElement(`img`);
+  img.src = toyObj[`image`];
+  img.className = `toy-avatar`;
+  const p = document.createElement(`p`);
+  p.textContent = `${toyObj[`likes`]} Likes`;
+  const btn = document.createElement(`button`);
+  btn.className = `like-btn`;
+  btn.id = toyObj[`id`];
+  div.append(h2, img, p, btn);
+  document.querySelector(`div#toy-collection`).append(div);
+
+  btn.addEventListener(`click`, () => {
+    toyObj[`likes`] ++;
+    p.textContent = `${toyObj[`likes`]} Likes`;
+    updateToyLikes(toyObj)
+  })
 }
